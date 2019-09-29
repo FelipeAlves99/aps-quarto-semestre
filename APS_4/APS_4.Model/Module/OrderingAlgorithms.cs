2457e54beaf1998fd624cf648c07b910195dd68f
FelipeAlves99 <falves99.dev@gmail.com>
2019-09-28 21:48:47 -0300

    Implementação da interface IOrderingAlgorithms a classe

--- APS_4/APS_4.Model/Module/OrderingAlgorithms.cs
+++ APS_4/APS_4.Model/Module/OrderingAlgorithms.cs
@@ -1,13 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using APS_4.Model.Entities;
+using APS_4.Model.Interfaces;
+using System;
 
 namespace APS_4.Model.Module
 {
-    public class OrderingAlgorithms
+    public class OrderingAlgorithms : IOrderingAlgorithms
     {
+        public OrderingEntity BubbleSort(OrderingEntity entity)
+        {
+            throw new NotImplementedException();
+        }
 
+        public OrderingEntity InsertionSort(OrderingEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrderingEntity MergeSort(OrderingEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrderingEntity QuickSort(OrderingEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrderingEntity SelectionSort(OrderingEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrderingEntity ShellSort(OrderingEntity entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
