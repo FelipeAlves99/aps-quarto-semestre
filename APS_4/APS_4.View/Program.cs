e7aede0669f73df148420a4ca3fbb7a3e7830317
Felipe Alves <falves99.dev@gmail.com>
2019-10-05 13:16:29 -0300

    Selection sort e tela de teste (#2)
    * Feito o método de ordenação SelectionSort
    
    * Criação da tela de teste
    
    * Criação dos métodos da tela de teste

--- APS_4/APS_4.View/Program.cs
+++ APS_4/APS_4.View/Program.cs
@@ -16,7 +16,7 @@ namespace APS_4.View
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new TestForm());
         }
     }
 }
