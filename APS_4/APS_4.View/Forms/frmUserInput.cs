33fa9c87b4198605c21da29dbb0baeea6d4870f6
FelipeAlves99 <falves99.dev@gmail.com>
2019-11-08 21:27:38 -0300

    Criado o design do formulário de entrada de dados

---
+++ APS_4/APS_4.View/Forms/frmUserInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APS_4.View.Forms
+{
+    public partial class frmUserInput : Form
+    {
+        public frmUserInput()
+        {
+            InitializeComponent();
+        }
+    }
+}
